83e26762054a01f75fa72951731ea89aa6a65022
faezeam <faezeam@users.noreply.github.com>
2020-01-03 20:04:49 +0330

    login view

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -9,20 +9,25 @@ label {
 }​
 </style>
 <div class="container">
+<div class="row">
+    <div class="col-12 p-5 d-flex justify-content-center  " >
+    <img src="/back.jpg" style="width:300px;" >
+    </div>
+    </div>
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('ورود') }}</div>
+                
 
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
 
                         <div class="form-group row ">
-                            <label for="username" class="col-md-4 col-form-label text-md-right">{{ __('نام کاربری') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="username" style="text-align:right;" type="text" placeholder="نام کاربری" class="form-control @error('username') is-invalid @enderror" name="username" value="{{ old('username') }}" required autocomplete="username" autofocus>
+                            
+                            <div class="col-3"></div>
+                            <div class="col-md-4">
+                                <input id="username" style="text-align:right; width:300px;" type="text" placeholder="نام کاربری" class="form-control @error('username') is-invalid @enderror" name="username" value="{{ old('username') }}" required autocomplete="username" autofocus>
 
                                 @error('username')
                                     <span class="invalid-feedback" role="alert">
@@ -33,10 +38,11 @@ label {
                         </div>
 
                         <div class="form-group row">
-                            <label for="password" class="col-md-4 col-form-label text-md-right " >{{ __('کلمه عبور') }}</label>
+                            
+                        <div class="col-3"></div>
 
-                            <div class="col-md-6">
-                                <input id="password" style="text-align:right;" placeholder="کلمه عبور" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                            <div class="col-md-4">
+                                <input id="password" style="text-align:right; width:300px;" placeholder="کلمه عبور" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -47,10 +53,10 @@ label {
                         </div>
 
                         <div class="form-group row">
-                            <div class="col-md-6 offset-md-4">
+                            <div class=" offset-md-4 d-flex">
+                            <div class="mr-5  "><a class="btn btn-primary " style="background-color:#159B9F; font-family: BYekan;" href="/register">ثبت نام</a> </div>
                                 <div class="form-check">
                                     <input class="form-check-input" style="text-align:right;" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
                                     <label class="form-check-label" for="remember">
                                         {{ __('من را به خاطر بسپار') }}
                                     </label>
@@ -59,16 +65,13 @@ label {
                         </div>
 
                         <div class="form-group row mb-0">
-                            <div class="col-md-8 offset-md-4 ">
-                                <button type="submit" class="btn btn-primary">
+                            <div class="col-1"></div>
+                            <div class="col-4 offset-md-4 ">
+                                <button type="submit" style="background-color:#159B9F; font-family: BYekan;" class="btn btn-primary">
                                     {{ __('ورود') }}
                                 </button>
 
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('رمز عبور خود را فراموش کرده اید؟') }}
-                                    </a>
-                                @endif
+
                             </div>
                         </div>
                     </form>
